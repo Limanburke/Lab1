05409751adcc1059ee0e253038ced64bde9587f0
Eugene <kryvushkoyevhenii@knu.ua>
2025-09-12 23:13:01 +0300

    start of test

--- board.cpp
+++ board.cpp
@@ -10,13 +10,13 @@ void Board::mark_cell(int index) {
 
 int Board::free_zone_size() {
     
-    
+    return 0;
 }
 
 double Board::average_free_zone() {
-
+    return 0;
 }
 
 double Board::median_free_zone() {
-
+    return 0;
 }
