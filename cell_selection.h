e7e76affd20be5631ef2ea388224fc4c77ad5445
Yulia0627 <repetiy0905@gmail.com>
2025-10-05 12:44:20 +0300

    Update cell_selection.h

--- cell_selection.h
+++ cell_selection.h
@@ -2,13 +2,13 @@
 #include <random>
 #include <vector>
 
-class Cell_chose {
+class Cell_selection {
 private:
     std::mt19937 gen;
     std::uniform_int_distribution<> dist;
     std::vector<int> random_numbers;
 public:
-    Cell_chose(int size);
+    Cell_selection(int size);
     int operator() ();
     void generate_random_numbers(int marks, int num_experiment);
     std::vector<int>& get_random_numbers();
